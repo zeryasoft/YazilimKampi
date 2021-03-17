f2c2657a72dc821dfbf86832e871c5b84ad909bc
zerya <zerya@ZANA>
2021-03-18 00:56:48 +0300

    ClassMetotDemo

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+
+        }
+
+        public void GetAll(Musteri musteri)
+        {
+
+        }
+
+        public void Update(Musteri musteri)
+        {
+
+        }
+    }
+}
