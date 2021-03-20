7d4e3531925fb20c3e98ca42ecd2e4c02b7a63de
zerya <zerya@ZANA>
2021-03-20 16:02:09 +0300

    dictionary

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    public class MyDictionary<TKey,TValue>
+    {
+        TKey[] _Key;
+        TValue[] _Value;
+        public MyDictionary()
+        {
+            _Key = new TKey[0];
+            _Value = new TValue[0];
+        }
+
+        public void Add(TKey key,TValue value)
+        {
+            TKey[] tempArrayKey = _Key;
+            TValue[] temArrayValue = _Value;
+
+            _Key = new TKey[_Key.Length + 1];
+            _Value = new TValue[_Value.Length + 1];
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                _Key[i] = tempArrayKey[i];
+                _Value[i]= temArrayValue[i];
+            }
+
+            _Key[_Key.Length - 1] = key;
+            _Value[_Value.Length - 1] = value;
+        }
+
+        public TValue[] value
+        {
+            get { return _Value; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Sedat");
+            myDictionary.Add(1, "Nihal");
+            myDictionary.Add(1, "Enes");
+            myDictionary.Add(1, "Zana");
+            myDictionary.Add(1, "Zerya");
+
+            foreach (var item in myDictionary.value)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
