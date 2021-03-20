cf271d0ce5e8b03943e9d77ae95b1b59f5eb04fb
zerya <zerya@ZANA>
2021-03-20 16:03:49 +0300

    dictionary

--- MyDictionary/Program.cs
+++ MyDictionary/Program.cs
@@ -8,10 +8,10 @@ namespace MyDictionary
         {
             MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
             myDictionary.Add(1, "Sedat");
-            myDictionary.Add(1, "Nihal");
-            myDictionary.Add(1, "Enes");
-            myDictionary.Add(1, "Zana");
-            myDictionary.Add(1, "Zerya");
+            myDictionary.Add(2, "Nihal");
+            myDictionary.Add(3, "Enes");
+            myDictionary.Add(4, "Zana");
+            myDictionary.Add(5, "Zerya");
 
             foreach (var item in myDictionary.value)
             {
