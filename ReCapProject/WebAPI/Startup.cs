0246ef353f47471972ab8a2d2d40edad6e52a08c
zerya <zerya@ZANA>
2021-04-17 01:32:58 +0300

    17.04.2021

--- ReCapProject/WebAPI/Startup.cs
+++ ReCapProject/WebAPI/Startup.cs
@@ -30,7 +30,9 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            //AOP
+            //Autofac, Ninject, CastWindsor,StructureMap, DryInject
+            //PostSharp
             services.AddControllers();
             services.AddSingleton<ICarService, CarManager>();
             services.AddSingleton<ICarDal,EfCarDal>();
